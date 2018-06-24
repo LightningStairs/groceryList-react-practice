2cc0dfa85920ff833d83e1df17804bb4fd44f92b
LightningStairs <jenah.blitz@icloud.com>
2018-06-24 17:03:20 -0400

    create groceries components array and add groceries to the return

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -2,21 +2,19 @@ import React from 'react';
 import Grocery from './Grocery';
 
 const GroceryList = props => {
+  let groceryItems = props.data;
+  let groceries = groceryItems.map(item => {
+    return(
+      <Grocery
+        key={item.id}
+        name={item.name}
+      />
+    )
+  })
   return(
   <div>
     <ul>
-      <li>
-        Oranges
-        <button type="button" onClick={ (event) => { alert('Button was clicked')} }>Delete</button>
-      </li>
-      <li>
-        Bananas
-        <button type="button" onClick={ (event) => { alert('Button was clicked')} }>Delete</button>
-      </li>
-      <li>
-        Bread
-        <button type="button" onClick={ (event) => { alert('Button was clicked')} }>Delete</button>
-      </li>
+      {groceries}
     </ul>
   </div>
 )
