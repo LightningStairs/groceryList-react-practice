2a6978f75068ac3301ef818be7d769303dd47bb2
LightningStairs <jenah.blitz@icloud.com>
2018-06-24 16:50:33 -0400

    add groceryData array to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import GroceryList from './GroceryList';
 const App = props => {
+  let groceryData = [
+   { id: 1, name: "Oranges" },
+   { id: 2, name: "Bananas" },
+   { id: 3, name: "Bread" }
+  ];
+
   return(
     <div>
       <h1>Grocery List</h1>
